6fa15f82b5b0bdd14fcd7670bc9c9728ad00a4bd
zofiat117 <154427695+zofiat117@users.noreply.github.com>
2024-11-26 10:01:17 +0000

    Add files via upload
    zosia, quizpage code

---
+++ questions.js
@@ -0,0 +1,12 @@
+const questions = [
+    { question: "", answer: "" },
+    { question: "", answer: "Paris" },
+    { question: "", answer: "" },
+    { question: "", answer: "" },
+    { question: "", answer: "" },
+    { question: "", answer: "" },
+    { question: "", answer: "" },
+    { question: "", answer: "" },
+    { question: "", answer: "" },
+    { question: "", answer: "" }
+];
